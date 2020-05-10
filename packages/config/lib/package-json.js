16dc3046d7f6c0b947b343ed9b1fa335f41c5c28
Adrien Duchemin <duchemin.adrien@gmail.com>
2020-05-10 04:48:32 +0200

    tiny fix

--- packages/config/lib/package-json.js
+++ packages/config/lib/package-json.js
@@ -1,18 +1,22 @@
-import { readFileSync, writeFileSync } from 'fs'
-import { resolve } from 'path'
+'use strict'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
 
 const loadJsonFile = (filepath) => {
-  const rawData = readFileSync(filepath)
+  const rawData = fs.readFileSync(filepath)
   return JSON.parse(rawData.toString())
 }
 
 const saveJsonFile = (filepath, data) => {
   const content = `${JSON.stringify(data, null, '  ')}\n`
-  writeFileSync(filepath, content)
+  fs.writeFileSync(filepath, content)
 }
 
 export const packageJson = (parentFolder) => {
-  const parentPackagePath = resolve(parentFolder, 'package.json')
+  const parentPackagePath = path.resolve(parentFolder, 'package.json')
   const parentPackage = loadJsonFile(parentPackagePath)
   Object.assign(parentPackage.scripts, {
     lint: 'eslint . --ext js,ts,js,jsx,json --fix'
