37498ab9805392a7a493ea98be8658ea7fbc77ab
Adrien Duchemin <duchemin.adrien@gmail.com>
2020-05-10 16:21:40 +0200

    fix devDeps

--- packages/config/lib/package-json.js
+++ packages/config/lib/package-json.js
@@ -27,8 +27,6 @@ const update = (parentFolder) => {
 
   // should also remove format script because no more prettier
   Object.assign(parentPackage.scripts, {
-    format: undefined,
-    lint: 'eslint . --ext js,ts,js,jsx,json --fix',
     test: 'jest',
     'test:watch': 'jest --watch',
     'test:cov': 'jest --coverage',
@@ -36,27 +34,27 @@ const update = (parentFolder) => {
       'node --inspect-brk -r tsconfig-paths/register -r ts-node/register node_modules/.bin/jest --runInBand'
   })
 
-  Object.assign(parentPackage.devDependencies, {
-    '@company-starter/eslint-config': '1.0.7',
-    '@company-starter/jest-config': '1.0.4',
-    '@company-starter/ts-config': '1.0.4',
-    '@types/jest': '25.2.1',
-    '@types/node': '13.9.1',
-    '@types/supertest': '2.0.8',
-    '@typescript-eslint/eslint-plugin': '2.31.0',
-    '@typescript-eslint/parser': '2.31.0',
-    eslint: '6.8.0',
-    'eslint-config-prettier': '6.11.0',
-    'eslint-plugin-import': '2.20.2',
-    'eslint-plugin-prettier': '3.1.3',
-    jest: '25.1.0',
-    prettier: '2.0.5',
-    supertest: '4.0.2',
-    'ts-jest': '25.2.1',
-    'ts-node': '8.6.2',
-    'tsconfig-paths': '3.9.0',
-    typescript: '3.7.4'
-  })
+  // Object.assign(parentPackage.devDependencies, {
+  //   '@company-starter/eslint-config': '1.0.7',
+  //   '@company-starter/jest-config': '1.0.4',
+  //   '@company-starter/ts-config': '1.0.4',
+  //   '@types/jest': '25.2.1',
+  //   '@types/node': '13.9.1',
+  //   '@types/supertest': '2.0.8',
+  //   '@typescript-eslint/eslint-plugin': '2.31.0',
+  //   '@typescript-eslint/parser': '2.31.0',
+  //   eslint: '6.8.0',
+  //   'eslint-config-prettier': '6.11.0',
+  //   'eslint-plugin-import': '2.20.2',
+  //   'eslint-plugin-prettier': '3.1.3',
+  //   jest: '25.1.0',
+  //   prettier: '2.0.5',
+  //   supertest: '4.0.2',
+  //   'ts-jest': '25.2.1',
+  //   'ts-node': '8.6.2',
+  //   'tsconfig-paths': '3.9.0',
+  //   typescript: '3.7.4'
+  // })
 
   Object.assign(parentPackage.jest, {
     preset: '@company-starter/jest-config/lib',
